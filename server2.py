b2df3906020469ff241756c58395f60aec4fbd6c
Crimson Scythe <crimsonscythestudios@gmail.com>
2020-12-28 22:55:20 +0100

    added mongodb

--- server2.py
+++ server2.py
@@ -1,12 +1,16 @@
-from datetime import datetime
+import datetime
 from flask import Flask, json
 from flask import request
 from flask.helpers import make_response
+from pymongo.common import validate_document_class
 from flask_cors import CORS, cross_origin
 from io import StringIO, BytesIO
 import pandas as pd
 import numpy as np
-from sklearn import linear_model
+from sklearn import linear_model, svm
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn import tree
 from flask import Flask, session
 from flask import jsonify
 from flask_session import Session
@@ -15,10 +19,16 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 import jwt
+import pymongo
 from functools import wraps
+from configparser import ConfigParser
 
 app = Flask(__name__)
 # app.secret_key = 'Secretkey'
+
+'''load config file'''
+config_object = ConfigParser()
+config_object.read('config.ini')
 
 app.config['SECRET_KEY']='Th1s1ss3cr3t'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///C:/Users/hasee/Downloads/sqlite-tools-win32-x86-3340000/sqlite-tools-win32-x86-3340000.library.db'
@@ -88,7 +98,7 @@ def login_user():
 
 @app.route('/users', methods=['GET'])
 @token_required
-def get_all_users():
+def get_all_users(self):
     users = Users.query.all()
     result=[]
 
@@ -105,27 +115,179 @@ def get_all_users():
 
 @app.route('/projects/', methods=['GET'])
 @token_required
-def get_all_projects():
+def get_all_projects(self):
+    client = pymongo.MongoClient(config_object['MONGO']['host'])
+    db = client.db
 
+    collections = db.list_collection_names()
+
+    if len(collections)==0:
+        return 'no projects'
+    else:
+        return jsonify({'projects': collections})
+
+# def test_models(df, models, column=None):
+#     if column:
     
 
-    pass
+        
+#     else:
+#         pass
 
 
-@app.route('/projects/<projname>', methods=['POST'])
+def train_models(df, column=None):
+
+    if column:
+        trained_models={}
+
+        models={
+            'linear': linear_model.LinearRegression(),
+            'svm': svm.SVR(),
+            'nn': KNeighborsRegressor(),
+            # 'dt': tree.DecisionTreeClassifier()
+        }
+
+        for name, model in models.items():
+            trained_models[f'{column}-{name}']=pickle.dumps(model.fit(df.drop(columns=[column]), df[column]))
+
+
+        return trained_models
+
+    else:
+
+        trained_models={}
+
+        models={
+            'linear': linear_model.LinearRegression(),
+            'svm': svm.SVR(),
+            'nn': KNeighborsRegressor()
+            # 'gpr': GaussianProcessRegressor(),
+            # 'dt': tree.DecisionTreeClassifier()
+        }
+
+        for column in df.columns:
+            for name, model in models.items():
+                trained_models[f'{column}-{name}'] = pickle.dumps(model.fit(df.drop(columns=[column]),df[column]))
+
+        return trained_models
+
+@app.route('/project/<projname>/models/<column>', methods=['GET', 'POST'])
 @token_required
-def create_project(projname):
-    pass
+def get_create_single_models(self, projname, column):
+    if request.method == 'POST':
+        df = pd.read_csv(BytesIO(request.files['file'].read()))
+        
+        df = df.select_dtypes(include=np.number)
 
-@app.route('/projects/<projname>/models/', methods=['GET'])
+        trained_models = train_models(df, column)
+
+        client = pymongo.MongoClient(config_object['MONGO']['host'])
+        db = client.db
+
+        
+
+        documents = db[f'{projname}'].insert_many([{'model-data': trained_models[key]} for key in trained_models])
+
+        return '200'
+
+    else:
+        pass
+    
+
+@app.route('/projects/<projname>/models/test/', methods=['GET', 'POST'])
 @token_required
-def get_all_models_for_project(projname):
-    pass
+def get_test_models_all(self, projname):
+    
+    if request.method == 'POST':
+
+        model_list = []
+
+        df = pd.read_csv(BytesIO(request.files['file'].read()))
+        df = df.select_dtypes(include=np.number)
+
+        client = pymongo.MongoClient(config_object['MONGO']['host'])
+        db = client.db
+
+        auth = request.authorization
+
+        user = Users.query.filter_by(name=auth.username).first()
+
+        models = db['models'].find({'$and': [{'user_id': user.public_id},{'projname': projname}]})
+
+        lst = list(models)
+        for l in lst:
+            # print(l['model-name'])
+            # print(type(l))
+            model_list.append(pickle.loads(l['model-data']))
+
+        # tested_models = test_models(models, df)
+        for j in model_list:
+            print(j)
+
+    return '200'
+
+@app.route('/projects/<projname>/models/', methods=['GET', 'POST'])
+@token_required
+def get_create_models(self, projname):
+
+    if request.method == 'POST':
+
+        df = pd.read_csv(BytesIO(request.files['file'].read()))
+ 
+        df=df.select_dtypes(include=np.number)        
+
+        trained_models = train_models(df)
+
+        client = pymongo.MongoClient(config_object['MONGO']['host'])
+        db = client.db
+
+        auth = request.authorization
+
+        user = Users.query.filter_by(name=auth.username).first()
+
+        db['projects'].insert_one({'name': f'{projname}', 'id': user.public_id})
+
+        # db['projects'].find({'id': user.public_id})[f'{}']
+
+        db['projects'].update({'id':user.public_id},  {'$set': {'models': [{'model-name': key} for key in trained_models]}})
+
+        db['models'].insert_many([{'projname': projname, 'user_id': user.public_id, 'model-name': key, 'model-data': trained_models[key]} for key in trained_models])
+
+
+        # db['projects'].update({'id':user.public_id},  {'models': [{'model-name': key, 'model-data': trained_models[key]} for key in trained_models]})
+
+        # documents = db['projects'].insert_many([{'model-name': key, 'model-data': trained_models[key]} for key in trained_models])
+
+        return '200'
+    else:
+        client = pymongo.MongoClient(config_object['MONGO']['host'])
+        db = client.db
+
+        documents = db[f'{projname}'].find()
+
+        print(documents)
+
+        return '200'
+        # if documents.count==0:
+        #     return 'No models'
+        # else:
+        #     jsonify({'models': documents})
 
 @app.route('/projects/<projname>/models/<modelname>', methods=['GET'])
 @token_required
-def get_model_for_project(projname, modelname):
-    pass
+def test_models(projname, modelname):
+
+    client = pymongo.MongoClient(config_object['MONGO']['host'])
+    db = client.db
+
+    model = db['projects'].find({'$and': [{'models': modelname},{"name": f"{projname}"}]})
+
+    # model = db[f'{projname}'].find({"name": f"{modelname}"})
+
+    if model:
+        return jsonify({"model": model})
+    else:
+        return 'No model'
 
 
 if __name__ == '__main__':
@@ -146,30 +308,30 @@ if __name__ == '__main__':
 # @app.route('/post/train', methods=['POST'])
 # def trainmodel():
     
-#     # print(request.files)
-#     # print(request.form)
+    # # print(request.files)
+    # # print(request.form)
     
-#     # print(request.files['file'].filename)
+    # # print(request.files['file'].filename)
 
-#     fil = request.files['file']
+    # fil = request.files['file']
 
-#     df = pd.read_csv(BytesIO(request.files['file'].read()))
-#     col = request.form['column_name']
+    # df = pd.read_csv(BytesIO(request.files['file'].read()))
+    # col = request.form['column_name']
     
-#     reg = linear_model.LinearRegression()
+    # reg = linear_model.LinearRegression()
 
-#     df=df.select_dtypes(include=np.number)
+    # df=df.select_dtypes(include=np.number)
 
-#     reg.fit(X=df.drop(columns=[col]), y=df[col])
+    # reg.fit(X=df.drop(columns=[col]), y=df[col])
     
 
-#     with open(f'{fil.filename[:-4]}.pickle', 'wb') as handle:
-#         pickle.dump(reg, handle, protocol=pickle.HIGHEST_PROTOCOL)    
+    # with open(f'{fil.filename[:-4]}.pickle', 'wb') as handle:
+    #     pickle.dump(reg, handle, protocol=pickle.HIGHEST_PROTOCOL)    
 
-#     # reg2 = pickle.loads(s)
-#     # preds=reg2.predict(df.drop(columns=[col]))
+    # # reg2 = pickle.loads(s)
+    # # preds=reg2.predict(df.drop(columns=[col]))
 
-#     return '200'
+    # return '200'
 
 # @app.route('/post/pred', methods=['POST'])
 # def pred():
