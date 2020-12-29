80895ed1b15572cee54a74246083225625db7d26
Crimson Scythe <crimsonscythestudios@gmail.com>
2020-12-29 21:17:16 +0100

    cleanup

--- src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const FILE_UPLOAD = 'FILE_UPLOAD'
-export const FILE_SET = 'FILE_SET'
-export const COLS_GET = 'COLS_GET'
-export const MODEL_TRAIN = 'MODEL_TRAIN'
-export const MODEL_PRED = 'MODEL_PRED'
